268afb01aa189cc4b2c38c4b1fdb3331bf34adf0
Lior Shiboli <lior12sh@gmail.com>
2023-05-15 00:57:13 +0300

    finished algorithm

--- adaline.py
+++ adaline.py
@@ -2,20 +2,38 @@ import pandas as pd
 import numpy as np
 def load_data(dirname):
     return pd.read_csv(dirname,index_col=0)
-def main():
-    df = load_data("preproceesed_data")
-    data_points =df.drop('0',axis=1).to_numpy()
-    answers = df['0'].to_numpy()
+def adaline_training(answers,data_points,alpha,epochs):
+    data_size = len(data_points)
     weights = np.random.rand(100)
     bias = np.random.rand()
-    epoch=0
-    
-    while epoch<100:
-        np.dot(weights,data_points.T)+bias
-        #update somehow
-        
+    for iteration in range(epochs):
+        predictions =np.dot(weights,data_points.T)+bias
+        error = predictions - answers
+        weights = weights -(1/data_size)*2*alpha*np.dot(error,data_points)
+        bias = bias - (1/data_size)*2*alpha*error.sum()
 
+
+    return (weights,bias)
+def main():
+    df = load_data("preproceesed_data")
+    while input("do you want to predict[Y/N]")=="Y":
+        to_predict1= int(input("which number do you want to be 1 in the predicition "))
+        to_predict0= int(input("which number do you want to be 0 in the predicition "))
+        df_relevant = df[df['0'].isin( [to_predict0,to_predict1])]
+        df_relevant = df_relevant.sample(frac=1).reset_index(drop=True)
+        data_points =  df_relevant.drop('0',axis=1).to_numpy()
+        answers = df_relevant['0'].to_numpy()
+        answers = (answers==1).astype(float)
+        index = np.arange(len(answers))
         
+        for i in range(5):
+            weights,bias = adaline_training(answers[index%5 !=i],data_points[index%5 !=i],0.01,10000)
+            training_predictions = np.rint(np.dot(weights,data_points[index%5 !=i].T)+bias)
+            test_predictions = np.rint(np.dot(weights,data_points[index%5 ==i].T)+bias)
+            training_accuracy = (training_predictions == answers[index%5 !=i]).sum()/len(answers[index%5!=i])
+            test_accuracy = (test_predictions == answers[index%5 ==i]).sum()/len(answers[index%5==i])
+            print("train accuracy = "+str(training_accuracy))
+            print("test accuracy = "+str(test_accuracy))
 
 if __name__ == "__main__":
     main()
